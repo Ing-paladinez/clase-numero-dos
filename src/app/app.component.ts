60a208562e93e583c8557efee25acee385765a24
Paladinez <paladinezindustries@gmail.com>
2025-08-29 22:10:54 -0500

    Subiendo proyecto Angular clase numero dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormArray,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'clase2';
+  formulario; // declaramos aquí
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      nombres: this.fb.array<FormControl<string>>([]),
+    });
+  }
+
+  get nombres(): FormArray<FormControl<string>> {
+    return this.formulario.get('nombres') as FormArray<FormControl<string>>;
+  }
+
+  agregarNombre() {
+    this.nombres.push(
+      this.fb.nonNullable.control('', { validators: [Validators.required] })
+    );
+  }
+
+  eliminarNombre(i: number) {
+    this.nombres.removeAt(i);
+  }
+
+ 
+
+
+  resultado: any = null;
+
+enviar() {
+  if (this.formulario.valid) {
+    this.resultado = this.formulario.value;  // Guardamos el valor
+  }
+}
+
 }
